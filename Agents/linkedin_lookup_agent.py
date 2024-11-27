e67a5a354622a9fb5b86db0663ffc8422d98714e
s3975993 <s3975993@student.rmit.edu.au>
2024-11-27 13:21:28 +1100

    Created Flask app

--- Agents/linkedin_lookup_agent.py
+++ Agents/linkedin_lookup_agent.py
@@ -20,7 +20,7 @@ def lookup(name:str)->str:
    llm=ChatOllama(model="llama3.2")
    
    template="""Given the full name {name_of_person} of the person. I want you to give me the link to their linkedin profile
-    Your answer should only be a URL.
+    Your answer should only be a URL which is ther URL of their linkedin page.
    """
    
    prompt_template=PromptTemplate(template=template,input_variables=["name_of_the_person"])
@@ -35,7 +35,7 @@ def lookup(name:str)->str:
    
    react_prompt=hub.pull("hwchase17/react")
    agent=create_react_agent(llm=llm,tools=tools_for_agent,prompt=react_prompt)
-   agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+   agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True, handle_parsing_errors=True)
 
    result = agent_executor.invoke(
         input={"input": prompt_template.format_prompt(name_of_person=name)}
@@ -45,4 +45,4 @@ def lookup(name:str)->str:
    return linked_profile_url
 
 if __name__ == "__main__":
-    print(lookup(name="Raj Sanshi CSIRO"))
+    print(lookup(name="Eden Marco Udemy Linkedin"))
--- ice_breaker.py
+++ ice_breaker.py
@@ -4,24 +4,37 @@ from langchain_core.output_parsers import StrOutputParser
 import time
 from Third_Parties.linkedin import scrape_linkedin_profile
 import os
+from dotenv import load_dotenv
+from Agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import summary_parser, Summary
+from typing import Tuple
 
 
-
-
-
-if __name__=="__main__":
-    print("Hello Langchain")
-
+def ice_break_with(name:str)->Tuple[Summary, str]:
+    #linkedin URL
+    linkedin_username=linkedin_lookup_agent(name=name)
+    linkedin_data=scrape_linkedin_profile(linkedin_profile_url=linkedin_username,mock=True)
+    
     summary_template="""
     given the linkedin information {information} about the person , I want you to create
     1. a short summary
     2. two interesting facts about them
+    
+    \n{format_instructions}
     """
-    summary_prompt_template=PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template=PromptTemplate(
+        input_variables=["information"], template=summary_template,
+         partial_variables={"format_instructions":summary_parser.get})
 
     llm=ChatOllama(model="llama3.2") # The API key will be automatically loaded
-    chain=summary_prompt_template|llm|StrOutputParser()
-    linkedin_data=scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/raj-sanshi-5b97bb141/")
-    res=chain.invoke(input={"information": linkedin_data})
-    time.sleep(1)
-    print(res)
+    chain=summary_prompt_template|llm|summary_parser
+    res:Summary=chain.invoke(input={"information": linkedin_data})
+    return res, linkedin_data.get("profile_pic_url")
+
+if __name__=="__main__":
+    load_dotenv()
+    print("Ice Break Enter")
+    ice_break_with(name="Eden Marco Udemy linkedin")
+    
+
+    
